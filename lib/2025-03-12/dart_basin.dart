1b7920c978a0da6cb1e9a7164745c5502d3af8c7
cho1taeho <cow12dog@gmail.com>
2025-03-12 17:26:03 +0900

    250312 과제

--- lib/2025-03-12/dart_basin.dart
+++ lib/2025-03-12/dart_basin.dart
@@ -1,13 +1,10 @@
 void main(){
-
   print(sumOfMultiples(10));
   print(sumOfMultiples(20));
-
   print(isPrime(7));
   print(isPrime(12));
   print(isPrime(23));
 }
-
 
 int sumOfMultiples(int limit){
   int ans = 0;
@@ -19,19 +16,18 @@ int sumOfMultiples(int limit){
   return ans;
 }
 
-
-bool isPrime(int number){
-  if (number < 2){
+bool isPrime(int number) {
+  if (number < 2) {
     return false;
   }
-  if( number == 2 || number == 3){
+  if (number == 2 || number == 3) {
     return true;
   }
-  if(number % 2 == 0){
+  if (number % 2 == 0) {
     return false;
   }
-  for (int i = 3; i * i <= number; i += 2){
-    if (number % i == 0){
+  for (int i = 3; i * i <= number; i += 2) {
+    if (number % i == 0) {
       return false;
     }
   }
